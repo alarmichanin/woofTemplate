fc7487f6b7256ad43fcd050c345060dd91bc5837
Roman Tkachenko <roman2003tk@gmail.com>
2022-04-23 17:53:32 +0300

    added category menu for small screen

--- globalScript.js
+++ globalScript.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     $(".barI .fa-bars").on("click", () => {
         $(".hamburger_menu").css("visibility", "visible").css("left", "0")
     })
-    $(".barI .fa-xmark").on("click", () => {
+    $(".barI .close_menu").on("click", () => {
         $(".hamburger_menu").css("visibility", "hidden").css("left", "-100%")
     })
 });
--- goodsPage/script.js
+++ goodsPage/script.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
-
-$("div.card,div.product").on("click",()=>{
-    window.location.href = "../productPage";
-})
-
+    $("div.card,div.product").on("click", () => {
+        window.location.href = "../productPage";
+    })
+    $(".filter i").on("click", () => {
+        $(".category_menu").css("visibility", "visible").css("left", "0")
+    })
+    $(".close_category").on("click", () => {
+        $(".category_menu").css("visibility", "hidden").css("left", "-100%")
+    })
 });
